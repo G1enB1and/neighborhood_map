fe8f891d5cddbacce63964e97444c9b2437958c5
G1enB1and <glen.bland.81@gmail.com>
2019-01-17 11:31:46 -0600

    basic map

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var map;
+
+function initMap() {
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7413549, lng: -73.9980244},
+    zoom: 13
+  });
+}
