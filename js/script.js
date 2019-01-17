888ef59f966173104d58b4975f0fdb88d300e82c
G1enB1and <glen.bland.81@gmail.com>
2019-01-17 11:47:16 -0600

    added first marker on map

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@ function initMap() {
     center: {lat: 40.7413549, lng: -73.9980244},
     zoom: 13
   });
+  
+  // lat longs to be used in markers
+  var tribeca = {lat: 40.719526, lng: -74.0089934};
+  
+  // markers
+  var marker = new google.maps.Marker({
+    position: tribeca,
+    map: map,
+    title: 'First Marker!'
+  });
+  
 }
