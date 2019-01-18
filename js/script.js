6546c854dea7f054249701c57ed85ec581729bf6
G1enB1and <glen.bland.81@gmail.com>
2019-01-18 10:18:21 -0600

    custom marker icons

--- js/script.js
+++ js/script.js
@@ -295,7 +295,7 @@ function initMap() {
             }
         ]
     }
-];
+]; // end of map styles array
 		
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
@@ -316,13 +316,6 @@ function initMap() {
     {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
   ];
   
-  // Style the markers
-  var defaultIcon = makeMarkerIcon('0091ff');
-
-  // Create a "highlighted location" marker color for when the user
-  // mouses over the marker.
-  var highlightedIcon = makeMarkerIcon('FFFF24');
-		
   var largeInfowindow = new google.maps.InfoWindow();
 
   // The following group uses the location array to create an array of markers on initialize.
@@ -336,7 +329,7 @@ function initMap() {
       position: position,
       title: title,
       animation: google.maps.Animation.DROP,
-	  icon: defaultIcon,
+	  icon: 'img/coffeeMarkerRed31x45.png',
       id: i
     });
 	
@@ -351,10 +344,10 @@ function initMap() {
 	// Two event listeners - one for mouseover, one for mouseout,
     // to change the colors back and forth.
     marker.addListener('mouseover', function() {
-      this.setIcon(highlightedIcon);
+      this.setIcon('img/coffeeMarkerYellow31x45.png');
     });
     marker.addListener('mouseout', function() {
-      this.setIcon(defaultIcon);
+      this.setIcon('img/coffeeMarkerRed31x45.png');
     });
 	
   } // end of for (var i = 0; i < locations.length; i++)
@@ -394,18 +387,4 @@ function hideListings() {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
   }
-}
-
-// This function takes in a COLOR, and then creates a new marker
-// icon of that color. The icon will be 21 px wide by 34 high, have an origin
-// of 0, 0 and be anchored at 10, 34).
-function makeMarkerIcon(markerColor) {
-  var markerImage = new google.maps.MarkerImage(
-    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-    '|40|_|%E2%80%A2',
-    new google.maps.Size(21, 34),
-    new google.maps.Point(0, 0),
-    new google.maps.Point(10, 34),
-    new google.maps.Size(21,34));
-  return markerImage;
 }
