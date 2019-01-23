7c363a158e3984664d40d877ffab9ec94d520b49
G1enB1and <glen.bland.81@gmail.com>
2019-01-22 18:21:35 -0600

    change style indent to 2 space

--- js/script.js
+++ js/script.js
@@ -8,138 +8,138 @@ function initMap() {
   // Create a styles array to use with the map.
   var styles = [
     {
-        "featureType": "administrative",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": "-100"
-            }
-        ]
+      "featureType": "administrative",
+      "elementType": "all",
+      "stylers": [
+        {
+          "saturation": "-100"
+        }
+      ]
     },
     {
-        "featureType": "administrative.province",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "administrative.province",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
     },
     {
-        "featureType": "landscape",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": 65
-            },
-            {
-                "visibility": "on"
-            }
-        ]
+      "featureType": "landscape",
+      "elementType": "all",
+      "stylers": [
+        {
+          "saturation": -100
+        },
+        {
+          "lightness": 65
+        },
+        {
+          "visibility": "on"
+        }
+      ]
     },
     {
-        "featureType": "poi",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "lightness": "50"
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [
+        {
+          "saturation": -100
+        },
+        {
+          "lightness": "50"
+        },
+        {
+          "visibility": "simplified"
+        }
+      ]
     },
     {
-        "featureType": "poi",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
     },
     {
-        "featureType": "road",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": "-100"
-            }
-        ]
+      "featureType": "road",
+      "elementType": "all",
+      "stylers": [
+        {
+          "saturation": "-100"
+        }
+      ]
     },
     {
-        "featureType": "road.highway",
-        "elementType": "all",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
+      "featureType": "road.highway",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "simplified"
+        }
+      ]
     },
     {
-        "featureType": "road.arterial",
-        "elementType": "all",
-        "stylers": [
-            {
-                "lightness": "30"
-            }
-        ]
+      "featureType": "road.arterial",
+      "elementType": "all",
+      "stylers": [
+        {
+          "lightness": "30"
+        }
+      ]
     },
     {
-        "featureType": "road.local",
-        "elementType": "all",
-        "stylers": [
-            {
-                "lightness": "40"
-            }
-        ]
+      "featureType": "road.local",
+      "elementType": "all",
+      "stylers": [
+        {
+          "lightness": "40"
+        }
+      ]
     },
     {
-        "featureType": "transit",
-        "elementType": "all",
-        "stylers": [
-            {
-                "saturation": -100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
+      "featureType": "transit",
+      "elementType": "all",
+      "stylers": [
+        {
+          "saturation": -100
+        },
+        {
+          "visibility": "simplified"
+        }
+      ]
     },
     {
-        "featureType": "water",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "hue": "#ffff00"
-            },
-            {
-                "lightness": -25
-            },
-            {
-                "saturation": -97
-            }
-        ]
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "hue": "#ffff00"
+        },
+        {
+          "lightness": -25
+        },
+        {
+          "saturation": -97
+        }
+      ]
     },
     {
-        "featureType": "water",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "lightness": -25
-            },
-            {
-                "saturation": -100
-            }
-        ]
+      "featureType": "water",
+      "elementType": "labels",
+      "stylers": [
+        {
+          "lightness": -25
+        },
+        {
+          "saturation": -100
+        }
+      ]
     }
-]; // end of map styles array
+  ]; // end of map styles array
 
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
