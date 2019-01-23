ce3a134ab4995a1907649cce02cab62e1673c5e5
G1enB1and <glen.bland.81@gmail.com>
2019-01-22 19:05:59 -0600

    start open brackets on same line as what they open
    follow style guide for open brackets. effected code is all withen styles

--- js/script.js
+++ js/script.js
@@ -6,140 +6,95 @@ let markers = [];
 function initMap() {
 	
   // Create a styles array to use with the map.
-  let styles = [
-    {
-      "featureType": "administrative",
-      "elementType": "all",
-      "stylers": [
-        {
-          "saturation": "-100"
-        }
-      ]
-    },
-    {
-      "featureType": "administrative.province",
-      "elementType": "all",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "landscape",
-      "elementType": "all",
-      "stylers": [
-        {
-          "saturation": -100
-        },
-        {
-          "lightness": 65
-        },
-        {
-          "visibility": "on"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "all",
-      "stylers": [
-        {
-          "saturation": -100
-        },
-        {
-          "lightness": "50"
-        },
-        {
-          "visibility": "simplified"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "labels.icon",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "featureType": "road",
-      "elementType": "all",
-      "stylers": [
-        {
-          "saturation": "-100"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "all",
-      "stylers": [
-        {
-          "visibility": "simplified"
-        }
-      ]
-    },
-    {
-      "featureType": "road.arterial",
-      "elementType": "all",
-      "stylers": [
-        {
-          "lightness": "30"
-        }
-      ]
-    },
-    {
-      "featureType": "road.local",
-      "elementType": "all",
-      "stylers": [
-        {
-          "lightness": "40"
-        }
-      ]
-    },
-    {
-      "featureType": "transit",
-      "elementType": "all",
-      "stylers": [
-        {
-          "saturation": -100
-        },
-        {
-          "visibility": "simplified"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "hue": "#ffff00"
-        },
-        {
-          "lightness": -25
-        },
-        {
-          "saturation": -97
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "labels",
-      "stylers": [
-        {
-          "lightness": -25
-        },
-        {
-          "saturation": -100
-        }
-      ]
-    }
-  ]; // end of map styles array
+  let styles = [{
+    "featureType": "administrative",
+    "elementType": "all",
+    "stylers": [{
+      "saturation": "-100"
+    }]
+  },{
+    "featureType": "administrative.province",
+    "elementType": "all",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },{
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [{
+      "saturation": -100
+    },{
+      "lightness": 65
+    },{
+      "visibility": "on"
+    }]
+  },{
+    "featureType": "poi",
+    "elementType": "all",
+    "stylers": [{
+      "saturation": -100
+    },{
+      "lightness": "50"
+    },{
+      "visibility": "simplified"
+    }]
+  },{
+    "featureType": "poi",
+    "elementType": "labels.icon",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },{
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [{
+      "saturation": "-100"
+    }]
+  },{
+    "featureType": "road.highway",
+    "elementType": "all",
+    "stylers": [{
+      "visibility": "simplified"
+    }]
+  },{
+    "featureType": "road.arterial",
+    "elementType": "all",
+    "stylers": [{
+      "lightness": "30"
+    }]
+  },{
+    "featureType": "road.local",
+    "elementType": "all",
+    "stylers": [{
+      "lightness": "40"
+    }]
+  },{
+    "featureType": "transit",
+    "elementType": "all",
+    "stylers": [{
+      "saturation": -100
+    },{
+      "visibility": "simplified"
+    }]
+  },{
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [{
+      "hue": "#ffff00"
+    },{
+      "lightness": -25
+    },{
+      "saturation": -97
+    }]
+  },{
+    "featureType": "water",
+    "elementType": "labels",
+    "stylers": [{
+      "lightness": -25
+    },{
+      "saturation": -100
+    }]
+  }]; // end of map styles array
 
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
