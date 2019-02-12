60d08c6b3438727fd6ce45914293dab6d941bd20
G1enB1and <glen.bland.81@gmail.com>
2019-02-12 17:05:29 -0600

    new backup

--- js/script_bkup.js
+++ js/script_bkup.js
@@ -5,39 +5,39 @@ markers = [];
 
 // These are the listings that will be shown to the user.
 coffeeShopLocations = ko.observableArray([
-  {title: 'Cafe Brazil 1', location: {lat: 32.844404, lng: -96.773435}},
-  {title: 'Cafe Brazil 2', location: {lat: 32.784975, lng: -96.783027}},
-  {title: 'Starbucks 1', location: {lat: 32.864403, lng: -96.660265}},
-  {title: 'Starbucks 2', location: {lat: 32.811152, lng: -96.623135}},
-  {title: 'Starbucks 3', location: {lat: 32.746236, lng: -96.585969}},
-  {title: 'Black Forest Coffee', location: {lat: 32.86609, lng: -96.764503}},
-  {title: 'Dennys 1', location: {lat: 32.819224, lng: -96.786784}},
-  {title: 'Dennys 2', location: {lat: 32.841681, lng: -96.593621}},
-  {title: 'Dennys 3', location: {lat: 32.789396, lng: -96.594197}},
-  {title: 'iHop 1', location: {lat: 32.857431, lng: -96.647735}},
-  {title: 'iHop 2', location: {lat: 32.859325, lng: -96.769432}},
-  {title: 'iHop 3', location: {lat: 32.768661, lng: -96.625545}},
-  {title: 'Goldmine', location: {lat: 32.876755, lng: -96.631224}},
-  {title: 'Beef House', location: {lat: 32.878382, lng: -96.647637}},
-  {title: 'Dunkin Donuts 1', location: {lat: 32.861236, lng: -96.643064}},
-  {title: 'Dunkin Donuts 2', location: {lat: 32.952197, lng: -96.769473}},
-  {title: 'White Rock Coffee', location: {lat: 32.864607, lng: -96.712334}}
+  {id: 1, title: 'Cafe Brazil 1', location: {lat: 32.844404, lng: -96.773435}},
+  {id: 2, title: 'Cafe Brazil 2', location: {lat: 32.784975, lng: -96.783027}},
+  {id: 3, title: 'Starbucks 1', location: {lat: 32.864403, lng: -96.660265}},
+  {id: 4, title: 'Starbucks 2', location: {lat: 32.811152, lng: -96.623135}},
+  {id: 5, title: 'Starbucks 3', location: {lat: 32.746236, lng: -96.585969}},
+  {id: 6, title: 'Black Forest Coffee', location: {lat: 32.86609, lng: -96.764503}},
+  {id: 7, title: 'Dennys 1', location: {lat: 32.819224, lng: -96.786784}},
+  {id: 8, title: 'Dennys 2', location: {lat: 32.841681, lng: -96.593621}},
+  {id: 9, title: 'Dennys 3', location: {lat: 32.789396, lng: -96.594197}},
+  {id: 10, title: 'iHop 1', location: {lat: 32.857431, lng: -96.647735}},
+  {id: 11, title: 'iHop 2', location: {lat: 32.859325, lng: -96.769432}},
+  {id: 12, title: 'iHop 3', location: {lat: 32.768661, lng: -96.625545}},
+  {id: 13, title: 'Goldmine', location: {lat: 32.876755, lng: -96.631224}},
+  {id: 14, title: 'Beef House', location: {lat: 32.878382, lng: -96.647637}},
+  {id: 15, title: 'Dunkin Donuts 1', location: {lat: 32.861236, lng: -96.643064}},
+  {id: 16, title: 'Dunkin Donuts 2', location: {lat: 32.952197, lng: -96.769473}},
+  {id: 17, title: 'White Rock Coffee', location: {lat: 32.864607, lng: -96.712334}}
 ]);
 
 selectedLocation = "";
-  
+
 // activate knockout.js and apply bindings for coffeeShopLocations
 // when all dependant DOM elements have been loaded and are ready.
 $(document).ready(function() {
   ko.applyBindings(coffeeShopLocations);
 });
- 
+
 /**
 * @description Initialize Map
 */
 function initMap() {
   let self = this;
-  
+
   // Create a styles array to use with the map.
   let styles = [{
     'featureType': 'administrative',
@@ -139,7 +139,7 @@ function initMap() {
 	styles: styles,
 	mapTypeControl: false
   });
-  
+
   largeInfowindow = new google.maps.InfoWindow();
 
   // The following group uses the location array to create an array of markers on initialize.
@@ -147,7 +147,7 @@ function initMap() {
     // Get the position from the location array.
     let position = coffeeShopLocations()[i].location;
     let title = coffeeShopLocations()[i].title;
-	
+
     // Create a marker per location, and put into markers array.
     marker = new google.maps.Marker({
       position: position,
@@ -156,25 +156,29 @@ function initMap() {
 	  icon: 'img/coffee_marker_green.png',
       id: i
     }); // end of marker = new google.maps.Marker({})
-	
+
     // Push the marker to our array of markers.
     markers.push(marker);
-	
+
     // Create an onclick event to open an infowindow at each marker.
     marker.addListener('click', function() {
 	  let self = this;
 	  selectedLocation = self;
-	  
-	  console.log(selectedLocation);
-	  
+
       populateInfoWindow(this, largeInfowindow);
-	  
+
 	  // set all marker icons back to green and remove animations
       for (let x = 0; x < markers.length; x++) {
         markers[x].setIcon('img/coffee_marker_green.png');
 	    markers[x].setAnimation(null);
       } // end of for()
-	  
+
+	  // remove locationTitleBold class from all titles elements
+	  for (let x = 1; x < markers.length+1; x++) {
+        let elem = document.getElementById(x);
+	    elem.classList.remove('locationTitleBold');
+      } // end of for()
+
 	  // set the selected location's marker icon to teal
       self.setIcon('img/coffee_marker_teal.png');
       // set the selected location's marker animation to bounce
@@ -182,8 +186,12 @@ function initMap() {
       // remove bounce animation after 1 bounce (700 ms)
       setTimeout(function(){ self.setAnimation(null); }, 700);
 
+	  // apply locationTitleBold class to selected location's title element in list
+	  let selectedLocationTitleElementById = document.getElementById(selectedLocation.id+1);
+	  selectedLocationTitleElementById.classList.add('locationTitleBold');
+
     }); // end marker.addListener(click)
-	
+
 	// Two event listeners - one for mouseover, one for mouseout,
     // to change the colors back and forth.
 	marker.addListener('mouseover', function() {
@@ -196,12 +204,12 @@ function initMap() {
         self.setIcon('img/coffee_marker_green.png');
 	  } // end if (marker != self)
     }); // end marker.addListener(mouseout)
-	
+
   } // end of for (var i = 0; i < locations.length; i++)
-	  
+
   // Show listings at load.
-  showListings(); 
-  
+  showListings();
+
 } // end of initMap()
 
 /**
@@ -212,23 +220,33 @@ selectLocation = function(coffeeShopLocation) {
   let self = this;
   let locationIndex = coffeeShopLocations().indexOf(coffeeShopLocation);
   selectedLocation = this;
-  
+
   // set all marker icons back to green and remove animations
   for (let i = 0; i < markers.length; i++) {
     markers[i].setIcon('img/coffee_marker_green.png');
 	markers[i].setAnimation(null);
   }
-  
+
+  // remove locationTitleBold class from all titles elements
+  for (let x = 1; x < markers.length+1; x++) {
+    let elem = document.getElementById(x);
+	elem.classList.remove('locationTitleBold');
+  } // end of for()
+
   // set the selected location's marker icon to teal
   markers[locationIndex].setIcon('img/coffee_marker_teal.png');
   // set the selected location's marker animation to bounce
   markers[locationIndex].setAnimation(google.maps.Animation.BOUNCE);
   // remove bounce animation after 1 bounce (700 ms)
   setTimeout(function(){ markers[locationIndex].setAnimation(null); }, 700);
-  
+
+  // apply locationTitleBold class to selected location's title element in list
+  let selectedLocationTitleElementById = document.getElementById(selectedLocation.id);
+  selectedLocationTitleElementById.classList.add('locationTitleBold');
+
   // Open InfoWindow at the marker for the location clicked
   populateInfoWindow(markers[locationIndex], largeInfowindow);
-  
+
 } // end of SelectLocation
 
 /**
@@ -242,22 +260,30 @@ function populateInfoWindow(marker, infowindow) {
     infowindow.marker = marker;
     infowindow.setContent('<div>' + marker.title + '</div>');
     infowindow.open(map, marker);
-	
+
     // Make sure the marker property is cleared if the infowindow is closed.
     infowindow.addListener('closeclick', function() {
       infowindow.marker = null;
     });
-	
+
 	// Close infowindow if empty map area is clicked
 	google.maps.event.addListener(map, "click", function(event) {
       infowindow.close();
 	  infowindow.marker = null;
+
 	  // set all marker icons back to green and remove animations
       for (let y = 0; y < markers.length; y++) {
         markers[y].setIcon('img/coffee_marker_green.png');
 	    markers[y].setAnimation(null);
-      }
-    });
+      } // end of for()
+
+	  // remove locationTitleBold class from all titles elements
+      for (let x = 1; x < markers.length+1; x++) {
+        let elem = document.getElementById(x);
+	    elem.classList.remove('locationTitleBold');
+      } // end of for()
+
+    }); // end of google.maps.event.addListener(map, "click", function(event)
 
   } // end of if(infowindow.marker != marker)
 } // end of populateInfoWindow()
@@ -283,3 +309,29 @@ function hideListings() {
     markers[i].setMap(null);
   }
 } // end of hideListings
+
+function filterFunction() {
+  // Declare variables
+  let filterInput, filter, i, txtValue;
+  filterInput = document.getElementById('txtFilterInput');
+  filter = filterInput.value.toUpperCase();
+
+  // get dom elements and text for each location in list
+  for (let x = 1; x < markers.length+1; x++) {
+    let elem = document.getElementById(x);
+    txtValue = elem.textContent || elem.innerText;
+
+    // compare input to text
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      // remove locationTitleHide class to show elements
+      elem.parentElement.classList.remove('locationTitleHide'); //show
+      // show map marker
+      markers[x-1].setMap(map);
+    } else {
+      // add locationTitleHide class to hide elements
+      elem.parentElement.classList.add('locationTitleHide'); //hide
+      // hide map marker
+      markers[x-1].setMap(null);
+    }
+  } // end of for()
+} // end of filterFunction()
