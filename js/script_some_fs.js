046ca48ccb7afb861fe212e0a7c7e9cc92048559
G1enB1and <glen.bland.81@gmail.com>
2019-02-17 11:43:52 -0600

    backing up

--- js/script_some_fs.js
+++ js/script_some_fs.js
@@ -32,6 +32,73 @@ $(document).ready(function() {
   ko.applyBindings(coffeeShopLocations);
 });
 
+let fsEndpoint = 'https://api.foursquare.com/v2/venues/search';
+let fsVersion = '20180323';
+let fsLL = '32.776664,-96.796988';
+let fsQuery = 'coffee';
+let fsLimit = '17';
+let fsClientID = 'E2QMBO1XOX1I3HM2TQ4BMEGVLA3ZHCHN1WG4RM40RGZJIZHH';
+let fsClientSecret = 'JSOKMIPYKJW52UBZDXRT3V1NUONCMIEWTJX3VTANTHY4NUC5';
+let fsParams = 'v=' + encodeURIComponent(fsVersion)
+  + '&' + 'll=' + encodeURIComponent(fsLL)
+  + '&' + 'query=' + encodeURIComponent(fsQuery)
+  + '&' + 'limit=' + encodeURIComponent(fsLimit)
+  + '&' + 'client_id=' + encodeURIComponent(fsClientID)
+  + '&' + 'client_secret=' + encodeURIComponent(fsClientSecret);
+let fsURL = fsEndpoint + '?' + fsParams;
+
+let getCoffeeShopsFromFS = new XMLHttpRequest();
+getCoffeeShopsFromFS.open('GET', fsURL);
+
+getCoffeeShopsFromFS.onload = function() {
+  let coffeeShopsFromFS = JSON.parse(getCoffeeShopsFromFS.responseText);
+  let coffeeShopVenues = coffeeShopsFromFS.response.venues;
+
+  //console.log(coffeeShopVenues);
+
+  assignCoffeeShops(coffeeShopVenues, coffeeShopLocations);
+
+}; // end of getCoffeeShopsFromFS
+
+getCoffeeShopsFromFS.send();
+
+function assignCoffeeShops(coffeeShopVenues, coffeeShopLocations) {
+
+  coffeeShopLocations.push({id: 18, title: 'TEST', location: {lat: 32.809224, lng: -96.796784}});
+  //setMarkers();
+  //showListings();
+
+  for (let i = 0; i < coffeeShopVenues.length; i++) {
+    coffeeShopLocations()[i].title = coffeeShopVenues[i].name;
+    //console.log(coffeeShopLocations()[i].title);
+  } // end of for()
+} // end of function assignCoffeeShops()
+
+
+/*
+function getFoursquare(){
+  let self = this;
+
+  $.ajax({
+    url: fsURL,
+    dataType: 'json',
+    success: function(data) {
+      venues = data.response.venues;
+
+      for (let i = 0; i < venues.length; i++) {
+        coffeeShopLocations()[i].title = venues[i].name;
+        console.log(coffeeShopLocations()[i].title);
+      } // end of for()
+
+    } // end of success
+  }); // end of $.ajax
+  // coffeeShopLocations()[0].title = 'TEST'; // works outside of $.ajax
+  // TODO: fix concurance or scope or async
+}; //  end of function getFoursquare()
+
+getFoursquare();
+*/
+
 /**
 * @description Initialize Map
 */
@@ -136,24 +203,34 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 32.776664, lng: -96.796988},
     zoom: 11,
-	styles: styles,
-	mapTypeControl: false
+	  styles: styles,
+	  mapTypeControl: false
   });
 
   largeInfowindow = new google.maps.InfoWindow();
 
+  setMarkers();
+
+  // Show listings at load.
+  showListings();
+
+} // end of initMap()
+
+function setMarkers() {
   // The following group uses the location array to create an array of markers on initialize.
   for (let i = 0; i < coffeeShopLocations().length; i++) {
     // Get the position from the location array.
     let position = coffeeShopLocations()[i].location;
     let title = coffeeShopLocations()[i].title;
 
+    //console.log(coffeeShopLocations()[i].title);
+
     // Create a marker per location, and put into markers array.
     marker = new google.maps.Marker({
       position: position,
       title: title,
       animation: google.maps.Animation.DROP,
-	  icon: 'img/coffee_marker_green.png',
+	    icon: 'img/coffee_marker_green.png',
       id: i
     }); // end of marker = new google.maps.Marker({})
 
@@ -162,55 +239,51 @@ function initMap() {
 
     // Create an onclick event to open an infowindow at each marker.
     marker.addListener('click', function() {
-	  let self = this;
-	  selectedLocation = self;
+	    let self = this;
+	    selectedLocation = self;
 
       populateInfoWindow(this, largeInfowindow);
 
-	  // set all marker icons back to green and remove animations
+	    // set all marker icons back to green and remove animations
       for (let x = 0; x < markers.length; x++) {
         markers[x].setIcon('img/coffee_marker_green.png');
-	    markers[x].setAnimation(null);
+	      markers[x].setAnimation(null);
       } // end of for()
 
-	  // remove locationTitleBold class from all titles elements
-	  for (let x = 1; x < markers.length+1; x++) {
+	    // remove locationTitleBold class from all titles elements
+	    for (let x = 1; x < markers.length+1; x++) {
         let elem = document.getElementById(x);
-	    elem.classList.remove('locationTitleBold');
+	      elem.classList.remove('locationTitleBold');
       } // end of for()
 
-	  // set the selected location's marker icon to teal
+	    // set the selected location's marker icon to teal
       self.setIcon('img/coffee_marker_teal.png');
       // set the selected location's marker animation to bounce
       self.setAnimation(google.maps.Animation.BOUNCE);
       // remove bounce animation after 1 bounce (700 ms)
       setTimeout(function(){ self.setAnimation(null); }, 700);
 
-	  // apply locationTitleBold class to selected location's title element in list
-	  let selectedLocationTitleElementById = document.getElementById(selectedLocation.id+1);
-	  selectedLocationTitleElementById.classList.add('locationTitleBold');
+	    // apply locationTitleBold class to selected location's title element in list
+	    let selectedLocationTitleElementById = document.getElementById(selectedLocation.id+1);
+	    selectedLocationTitleElementById.classList.add('locationTitleBold');
 
     }); // end marker.addListener(click)
 
-	// Two event listeners - one for mouseover, one for mouseout,
+	  // Two event listeners - one for mouseover, one for mouseout,
     // to change the colors back and forth.
-	marker.addListener('mouseover', function() {
-	  let self = this;
+	  marker.addListener('mouseover', function() {
+	    let self = this;
       self.setIcon('img/coffee_marker_teal.png');
     }); // end marker.addListener(mouseover)
-	marker.addListener('mouseout', function() {
-	  let self = this;
-	  if (selectedLocation != self) {
+	  marker.addListener('mouseout', function() {
+	    let self = this;
+	    if (selectedLocation != self) {
         self.setIcon('img/coffee_marker_green.png');
-	  } // end if (marker != self)
+	    } // end if (marker != self)
     }); // end marker.addListener(mouseout)
 
   } // end of for (var i = 0; i < locations.length; i++)
-
-  // Show listings at load.
-  showListings();
-
-} // end of initMap()
+} // end of setMarkers()
 
 /**
 * @description This function changes the marker icon of the selected location.
@@ -224,13 +297,13 @@ selectLocation = function(coffeeShopLocation) {
   // set all marker icons back to green and remove animations
   for (let i = 0; i < markers.length; i++) {
     markers[i].setIcon('img/coffee_marker_green.png');
-	markers[i].setAnimation(null);
+	  markers[i].setAnimation(null);
   }
 
   // remove locationTitleBold class from all titles elements
   for (let x = 1; x < markers.length+1; x++) {
     let elem = document.getElementById(x);
-	elem.classList.remove('locationTitleBold');
+	  elem.classList.remove('locationTitleBold');
   } // end of for()
 
   // set the selected location's marker icon to teal
